3294ebe721a067505e6d382b897ad974b2b19787
Asitmondal <mondalasit973@gmail.com>
2024-05-31 14:06:33 +0530

    Commit

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
     <header className="navbar">
       <div className="navbar__title">GetSocial</div>
       <nav className="navbar__links">
-        <a href="/profile">My Profile</a>
-        <a href="/dashboard">Home</a>
+        <a href="/dashboard">Feed</a>
         <a href="/create-post">Create Post</a>
-        <a href="/single-post">Single Post</a>
+        <a href="/profile">My Profile</a>
+        <a href="/settings">Settings</a>
         <a href="/logout">Logout</a>
       </nav>
     </header>
